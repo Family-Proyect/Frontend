3ce774f180749e4761110f3bc43df20d85ad8d3b
Luis Carrasco <cluis0152@gmail.com>
2020-11-07 00:11:10 -0500

    register user

--- family-proyect/src/environments/environment.prod.ts
+++ family-proyect/src/environments/environment.prod.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: true,
   apiUrl: "http://127.0.0.1:8000",
   imgUrl: "http://127.0.0.1:8000",
+  //apiUrl: "http://chjoguer.pythonanywhere.com",
+  //imgUrl: "http://chjoguer.pythonanywhere.com",
 
 
 };
